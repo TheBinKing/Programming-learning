2daab5495734cf428e6af563daa9a3d2ecf768a3
TheBinKing <1546697086@qq.com>
2023-06-21 11:38:21 +0800

    20230621

--- Leetcode-Python3/7.整数反转.py
+++ Leetcode-Python3/7.整数反转.py
@@ -31,15 +31,14 @@ class Solution:
         
         Args:
             x (int): _description_
-
         Returns:
-            int: _description_
-            
+            int: _description_  
         Your runtime beats 80.05 % of python3 submissions
         Your memory usage beats 18.62 % of python3 submissions (15 MB)
         """
         res=int(str(x)[::-1]) if x>=0 else -int(str(x)[1:][::-1])
         return 0 if res>pow(2,31)-1 or res<-pow(2,31) else res
+    
     def reverse0(self, x: int) -> int:
         """_summary_
         最简单的类型转换。
@@ -50,7 +49,6 @@ class Solution:
         -判断转换后的内容是否超过边界
         Args:
             x (int): _description_
-
         Returns:
             int: _description_
         """
@@ -60,6 +58,5 @@ class Solution:
         else:
             result=-int(str(abs(x))[::-1])
             return result if result>(-2**31) else 0
-
 # @lc code=end
 
